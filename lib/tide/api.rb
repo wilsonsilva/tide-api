99b145d76f1d96779618c90ce8feb22ce1f19cef
Wilson Silva <wilson.dsigns@gmail.com>
2019-05-26 12:20:46 +0100

    Document the main modules

--- lib/tide/api.rb
+++ lib/tide/api.rb
@@ -1,6 +1,8 @@
 require "tide/api/version"
 
+# Encapsulates all the code of the gem in a meaningful namespace.
 module Tide
+  # Operations related to Tide's API.
   module API
     class Error < StandardError; end
     # Your code goes here...
